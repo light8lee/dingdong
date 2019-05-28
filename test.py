d2b782e8514bee820b74ad5e7bb1f3c24d27e8c4
李涛 <17816873784@163.com>
2019-05-28 19:08:14 +0800

    fixed: wrong test

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ def test_wx_correct():
 
 @wx_reminder(enable_cmd_qr=2)
 def test_wx_exception():
-    raise("it's so terrible")
+    raise Exception("it's so terrible")
 
 
 @mqtt_reminder(host='localhost', port=1883, topic='dingdong', qos=1)
@@ -18,7 +18,7 @@ def test_mqtt_correct():
 
 @mqtt_reminder(host='localhost', port=1883, topic='dingdong', qos=1)
 def test_mqtt_exception():
-    raise("it's so terrible")
+    raise Exception("it's so terrible")
 
 if __name__ == '__main__':
     test_wx_correct()
